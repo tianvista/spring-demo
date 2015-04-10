2d9102fa239bf87ea73b405af72c1d872a1c826b
tianvista <tiandewei07@163.com>
2015-04-10 09:48:58 +0800

    hello world

---
+++ pb-example/pb-example-service/src/main/java/com/elong/pb/example/service/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.elong.pb.example.service;
+
+/**
+ * project：pb-example
+ * Date: 15/4/10
+ * Time: 09:46
+ * file: HelloWorld.java
+ *
+ * @author dewei.tian@corp.elong.com
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world ");
+    }
+}
